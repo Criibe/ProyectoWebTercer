60eb09abdbdb4fefb98a9af53f5eb62ff1478426
Cristian Salomon <csb@MacBook-Pro-de-M.local>
2017-12-03 18:33:40 -0600

    last

--- JS/reservaciones.js
+++ JS/reservaciones.js
@@ -2,6 +2,8 @@
 var id;
 var idUsuario;
 var idEvento = 0;
+var idEventosGuardados = [];
+var distribucion = []; 
 
 var DB = null;
 
@@ -19,21 +21,121 @@ $(document).ready(function cargarBase() {
 
 
 
-        if(idEvento != 0){
-
           DB.transaction(function(tran){
             tran.executeSql('SELECT rowid, * FROM EVENTOS ORDER BY rowid DESC LIMIT 1', [], function(tran, data){
                 idEvento = data.rows[0].rowid;
                 idEvento = idEvento+1;
-          });
-          });
-        } else idEvento = 1;
 
+          });
+          });
+
+        if(idEvento == 0)
+          idEvento = 1;
+        
+
+        /*Cargar los eventos anteriores*/
+        //Se cargan con respecto al id de usuario
+
+        DB.transaction(function(tran){
+                               
+          tran.executeSql('SELECT rowid, * FROM EVENTOS WHERE ID_USUARIO = "' + idUsuario + '";', [], function(tran, data){
+
+            for( i=0; i<data.rows.length; i++){
+              $('#eventoGuardado').append($('<option>', {
+                  value: i+1,
+                  text: data.rows[i].NOMBRE_EVENTO
+              }));
+              idEventosGuardados.push(data.rows[i].rowid);
+            }
+
+
+          });
+        });
+
+
+        $('#eventoGuardado').change(function(){
+
+          valor = $( "#eventoGuardado option:selected" ).text();
+          valorNum = $('#eventoGuardado').val()-1;
+
+          //Hacer las consultas a todo
+
+          //Tipo de evento seleccionado
+          $('#evento').val(convertirEventoANum(valor));
+
+          //Poner la fecha
+          DB.transaction(function(tran){
+                               
+          tran.executeSql('SELECT rowid, FECHA FROM EVENTOS WHERE NOMBRE_EVENTO = "' + valor + '";', [], function(tran, data){
+
+            f = data.rows[0].FECHA;
+            fechaRecibida = f.split("/");
+            dia = fechaRecibida[0];
+            mes = fechaRecibida[1];
+            año = fechaRecibida[2];
+
+            idEvento = data.rows[0].rowid;
+
+            if(parseInt(dia) <= 9)
+              dia = "0" + dia;
+
+          
+            $('#fecha').val(año + "-" + mes + "-" + dia);
+
+            });
+          });
+
+
+          //Hacer el reacomodo de los objetos
+
+           var matrizPosicion = [];
+
+           DB.transaction(function(tran){
+                               
+            tran.executeSql('SELECT * FROM DISTRIBUCION WHERE ID_EVENTO = ' + idEvento + ';', [], function(tran, data){
+
+            for( i=0; i<data.rows.length; i++){
+              distribucion.push(data.rows[i].ELEMENTO);
+            }
+
+
+           //Eliminar los elementos y agregarlos a la barra
+           for(i=0; i<3; i++){
+              for(j=0; j<3; j++){
+                var e = "#m" + (i+1) + "" + (j+1) + " img";
+                var el = $(e);
+                $('#dragBar').append(el);
+              }
+           }
+
+           //Agregar nuevamente la distribucion
+           alert(distribucion);
+           mostrarDistribucion();
+
+           //Poner los nombres
+            });
+           });
+
+
+                  
+        });
+
+        function mostrarDistribucion(){
+            it=0; 
+
+            for(i=0; i<3; i++){
+            for(j=0; j<3; j++){
+                var e = "#m" + (i+1) + "" + (j+1);
+                var n = "#" + distribucion[it];
+                $(e).append($(n));
+                it++;
+
+            }
+           }
+        }
 
 
         $('#reservar').click(function (event){
-
-
 
             var fecha = new Date($('#fecha').val());
             var dia = fecha.getDate();
@@ -41,8 +143,6 @@ $(document).ready(function cargarBase() {
             var año = fecha.getFullYear();
 
             var tipoEvento = $( "#evento option:selected" ).text();
-
-            alert("ID EVENTO " + idEvento);
 
             //ELEMENTOS DE LA MATRIZ
 
@@ -86,16 +186,13 @@ $(document).ready(function cargarBase() {
                 alert("Debes seleccionar una fecha");
               }
               else{
-                  alert("Opcion seleccionada: " + tipoEvento  + " Fecha: " + dia + "/" + mes + "/" + año );   
                   fechaFormada = dia + "/" + mes + "/" + año;
 
                   DB.transaction(function(tran){
-                      tran.executeSql('INSERT INTO EVENTOS (ID_USUARIO, NOMBRE_EVENTO, FECHA) VALUES ( "' + idUsuario +'", "' + tipoEvento + '", "' + fechaFormada + '");');
+                    tran.executeSql('INSERT INTO EVENTOS (ID_USUARIO, NOMBRE_EVENTO, FECHA) VALUES ( "' + idUsuario +'", "' + tipoEvento + '", "' + fechaFormada + '");');
                       
-                      //Insertar la distribucion del salon
-//******************************************
-                      for(i=0; i<3; i++){
-                        for(j=0; j<3; j++){
+                    for(i=0; i<3; i++){
+                      for(j=0; j<3; j++){
 
                           posicion = "#m" + (i+1) + "" + (j+1);
 
@@ -121,21 +218,21 @@ $(document).ready(function cargarBase() {
                           else
                             tran.executeSql('INSERT INTO DISTRIBUCION (ID_EVENTO,ELEMENTO, POSICION, PLATILLO) VALUES ("'+ idEvento +'", "'+ e +'", "'+ posicion +'", "'+ "--" +'");');
 
-                        }
                       }
+                     }
 
                       //Insertar el nombre de los invitados
-                      for(i=0; i<3; i++){
+                    for(i=0; i<3; i++){
 
-                        for(j=0; j<8; j++){
+                      for(j=0; j<8; j++){
 
-                            if(i==0){ //Invitados Mesa 1
-                              tran.executeSql('INSERT INTO INVITADOS (ID_EVENTO, MESA, NOMBRE_INVITADO) VALUES ("'+ idEvento +'", "'+ "mesa1" +'", "'+ nombresMesa1[j] +'") ;');
-                            }
-                            else if(i==1){
-                              tran.executeSql('INSERT INTO INVITADOS (ID_EVENTO, MESA, NOMBRE_INVITADO) VALUES ("'+ idEvento +'", "'+ "mesa2" +'", "'+ nombresMesa2[j] +'") ;');
-                            }
-                            else if(i==2){
+                        if(i==0){ //Invitados Mesa 1
+                            tran.executeSql('INSERT INTO INVITADOS (ID_EVENTO, MESA, NOMBRE_INVITADO) VALUES ("'+ idEvento +'", "'+ "mesa1" +'", "'+ nombresMesa1[j] +'") ;');
+                        }
+                        else if(i==1){
+                          tran.executeSql('INSERT INTO INVITADOS (ID_EVENTO, MESA, NOMBRE_INVITADO) VALUES ("'+ idEvento +'", "'+ "mesa2" +'", "'+ nombresMesa2[j] +'") ;');
+                        }
+                        else if(i==2){
                               tran.executeSql('INSERT INTO INVITADOS (ID_EVENTO, MESA, NOMBRE_INVITADO) VALUES ("'+ idEvento +'", "'+ "mesa3" +'", "'+ nombresMesa3[j] +'");');
                             }
                         }
@@ -143,6 +240,8 @@ $(document).ready(function cargarBase() {
                       }
 
                       alert("EVENTO GUARDADO ");
+
+                      location.reload();
                     });
               }
         });
@@ -181,6 +280,32 @@ function drop(event){
 	var offset = event.dataTransfer.getData("text/plain").split(",");
 
     return false;
+}
+
+function convertirEventoANum(ev){
+
+  switch(ev){
+    case "Bautizo": 
+      return 1;
+
+    case "Boda": 
+      return 2;
+
+    case "Graduación": 
+      return 3;
+     
+    case "Reunión de negocios": 
+      return 4;
+
+    case "Reunión familiar": 
+      return 5;
+
+    case "XV años": 
+      return 6;
+    
+    default:  
+      return -1;
+  }
 }
 
 
